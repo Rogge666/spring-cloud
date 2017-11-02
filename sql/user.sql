655c859163bc58e878c7bb009efc47ef888cca47
Rogge666 <297295350@qq.com>
2017-11-02 19:56:17 +0800

    1.修改sql

--- sql/user.sql
+++ sql/user.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50717
 File Encoding         : 65001
 
-Date: 2017-10-06 15:20:53
+Date: 2017-11-02 19:02:12
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -26,19 +26,23 @@ CREATE TABLE `user` (
   `nick_name` varchar(255) DEFAULT NULL,
   `sex` int(1) DEFAULT NULL,
   `register_date` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `email` varchar(255) NOT NULL,
+  `pass_word` varchar(255) NOT NULL,
+  `reg_time` varchar(255) NOT NULL,
+  `user_name` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO `user` VALUES ('1', '89921218@qq.com', '1ee04e0b1cb5af7367c80c22e42efd8b', '土豆', '1', '2017-06-23 14:24:23');
-INSERT INTO `user` VALUES ('2', '2@qq.com', '1ee04e0b1cb5af7367c80c22e42efd8b', '土豆-2', '1', '2017-06-23 14:24:23');
-INSERT INTO `user` VALUES ('3', '3@qq.com', '1ee04e0b1cb5af7367c80c22e42efd8b', '土豆-3', '1', '2017-06-23 14:24:23');
-INSERT INTO `user` VALUES ('4', '4@qq.com', '1ee04e0b1cb5af7367c80c22e42efd8b', '土豆-4', '1', '2017-06-23 14:24:23');
-INSERT INTO `user` VALUES ('5', '5@qq.com', '1ee04e0b1cb5af7367c80c22e42efd8b', '土豆-5', '1', '2017-06-23 14:24:23');
-INSERT INTO `user` VALUES ('6', '6@qq.com', '1ee04e0b1cb5af7367c80c22e42efd8b', '土豆-6', '1', '2017-06-23 14:24:23');
-INSERT INTO `user` VALUES ('7', '7@qq.com', '1ee04e0b1cb5af7367c80c22e42efd8b', '土豆-7', '1', '2017-06-23 14:24:23');
-INSERT INTO `user` VALUES ('8', '8@qq.com', '1ee04e0b1cb5af7367c80c22e42efd8b', '土豆-8', '1', '2017-06-23 14:24:23');
-INSERT INTO `user` VALUES ('9', '9@qq.com', '1ee04e0b1cb5af7367c80c22e42efd8b', '土豆-9', '1', '2017-06-23 14:24:23');
-INSERT INTO `user` VALUES ('10', '10@qq.com', '1ee04e0b1cb5af7367c80c22e42efd8b', '土豆-10', '1', '2017-06-23 14:24:23');
+INSERT INTO `user` VALUES ('1', '89921218@qq.com', '1ee04e0b1cb5af7367c80c22e42efd8b', '土豆', '1', '2017-06-23 14:24:23', '', '', '', '');
+INSERT INTO `user` VALUES ('2', '2@qq.com', '1ee04e0b1cb5af7367c80c22e42efd8b', '土豆-2', '1', '2017-06-23 14:24:23', '', '', '', '');
+INSERT INTO `user` VALUES ('3', '张三', '1ee04e0b1cb5af7367c80c22e42efd8b', '土豆-3', '1', '2017-11-01 20:54:26', '', '', '', '');
+INSERT INTO `user` VALUES ('4', '李四', '1ee04e0b1cb5af7367c80c22e42efd8b', '土豆-4', '1', '2017-11-01 20:54:26', '', '', '', '');
+INSERT INTO `user` VALUES ('5', '吴雷', '1ee04e0b1cb5af7367c80c22e42efd8b', '土豆-5', '1', '2017-11-01 20:54:26', '', '', '', '');
+INSERT INTO `user` VALUES ('6', '6@qq.com', '1ee04e0b1cb5af7367c80c22e42efd8b', '土豆-6', '1', '2017-06-23 14:24:23', '', '', '', '');
+INSERT INTO `user` VALUES ('7', '7@qq.com', '1ee04e0b1cb5af7367c80c22e42efd8b', '土豆-7', '1', '2017-06-23 14:24:23', '', '', '', '');
+INSERT INTO `user` VALUES ('8', '8@qq.com', '1ee04e0b1cb5af7367c80c22e42efd8b', '土豆-8', '1', '2017-06-23 14:24:23', '', '', '', '');
+INSERT INTO `user` VALUES ('9', '9@qq.com', '1ee04e0b1cb5af7367c80c22e42efd8b', '土豆-9', '1', '2017-06-23 14:24:23', '', '', '', '');
+INSERT INTO `user` VALUES ('10', '10@qq.com', '1ee04e0b1cb5af7367c80c22e42efd8b', '土豆-10', '1', '2017-06-23 14:24:23', '', '', '', '');
